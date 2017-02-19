646269be6d35ecf247747f434b97d7ce8415d2b3
Alex <kpecmuk@gmail.com>
2017-02-19 04:05:11 +0300

    19.02.17 04:04

--- src/main/java/ru/kpecmuk/sort/Main.java
+++ src/main/java/ru/kpecmuk/sort/Main.java
@@ -3,19 +3,22 @@ package ru.kpecmuk.sort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 /**
  * Проверки сортировки слиянием
  * Created by Kpecmuk on 10.02.2017.
  */
 public class Main {
-    private static Logger log = LoggerFactory.getLogger(Main.class.getSimpleName());
+    private static final Logger log
+            = LoggerFactory.getLogger(Main.class.getSimpleName());
 
     public static void main(String[] args) {
 
-        log.info(" --- Inside main --- " + log.getClass().getSimpleName());
-        MergeSort mergeSort = new MergeSort(new int[]{10, 1, 9, 2, 8, 3, 7, 4, 6, 5});
 
-        mergeSort.ShowArray();
+        MergeSort mergeSort = new MergeSort(new int[]{10, 1, 9, 2, 8, 3, 7, 4, 6, 5});
+        log.info(" --- Inside main --- Array : " + Arrays.toString(mergeSort.getArray()) + " created");
+
         mergeSort.Sort(mergeSort.getArray());
         mergeSort.ShowArray();
     }
